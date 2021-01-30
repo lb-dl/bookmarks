1459d66e243b8fb87d3de87ec8eafdca5cb1183f
lb-dl <lb_dl@i.ua>
2021-01-30 14:35:36 +0200

    built a model for custom user

--- src/bookmarks/settings.py
+++ src/bookmarks/settings.py
@@ -120,3 +120,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
